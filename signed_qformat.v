15077387c16a9e72e226c4d40a15bc91cdb7dd25
Dan McLeran <danmcleran@gmail.com>
2025-06-15 06:14:57 -0600

    Specify both fixed and fractional bits as module parameters.

--- signed_qformat.v
+++ signed_qformat.v
@@ -20,9 +20,26 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-module qformat #(parameter NUM_FIXED_BITS = 8);
-    reg signed [NUM_FIXED_BITS:0] value;
-    reg signed [((2*NUM_FIXED_BITS) - 1):0] product;
+module qformat #(parameter NUM_FIXED_BITS = 8, NUM_FRACTIONAL_BITS = 8)
+    (
+        input clock, reset,
+        output signed [NUM_FIXED_BITS + NUM_FRACTIONAL_BITS - 1:0] qvalue
+    );
+    // Define the total number of bits for value and product
+    // and the maximum bit indices for value and product
+    // The value is represented as a signed fixed-point number
+    // with NUM_FIXED_BITS for the integer part and NUM_FRACTIONAL_BITS for the fractional part
+    // The product is represented as a signed fixed-point number
+    // with double the total number of bits to accommodate multiplication results
+    // The POW constant is used to scale the fixed-point representation
+    // by 2 raised to the negative number of fixed bits, allowing for proper scaling in calculations
+    localparam TOTAL_NUM_BITS = NUM_FIXED_BITS + NUM_FRACTIONAL_BITS;
+    localparam MAX_VALUE_BIT = TOTAL_NUM_BITS - 1;
+    localparam TOTAL_NUM_PRODUCT_BITS = (2 * TOTAL_NUM_BITS);
+    localparam MAX_PRODUCT_BIT = TOTAL_NUM_PRODUCT_BITS - 1;
+
+    reg signed [MAX_VALUE_BIT:0] value;
+    reg signed [MAX_PRODUCT_BIT:0] product;
     
     localparam  POW = 2.0**(-1.0 * NUM_FIXED_BITS);
 
